88061cc23b282ff4ee16386ba486490fff77225e
tulsipratiba <sudhakartulsi@gmail.com>
2017-05-16 18:12:25 +0530

    assignment 1

---
+++ src/Design1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Design1 {
+
+	
+	public static void main(String[] args) 
+	{
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter the number of rows=column");
+        int n = s.nextInt();
+        int i, j;
+        s.close();
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (j == i) {
+                    System.out.print("*");
+                } else if (j == n - (i - 1)) {
+                    System.out.print("*");
+                } else 
+                {
+                	
+                    System.out.print("_");
+                }
+            }
+            System.out.println();
+            
+        }}}
