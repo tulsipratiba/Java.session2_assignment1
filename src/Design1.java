1605f301c54f5ca52dea02cbfdffb4fdefc99b16
tulsipratiba <sudhakartulsi@gmail.com>
2017-05-16 18:19:57 +0530

    update

--- src/Design1.java
+++ src/Design1.java
@@ -4,15 +4,22 @@ public class Design1 {
 	public static void main(String[] args) 
 	{
         Scanner s = new Scanner(System.in);
-        System.out.println("enter the number of rows=column");
+        
+        System.out.println("enter the number of rows=column"); // 
+        
         int n = s.nextInt();
-        int i, j;
+        
+        int i, j; // variable of type intezer
         s.close();
-        for (i = 1; i <= n; i++) {
-            for (j = 1; j <= n; j++) {
-                if (j == i) {
-                    System.out.print("*");
-                } else if (j == n - (i - 1)) {
+        for (i = 1; i <= n; i++)  //increment the value of i till n
+        {
+            for (j = 1; j <= n; j++) // for loop begins when j=0 and runs till j <=n
+            {
+                if (j == i) 
+                {
+                    System.out.print("*"); //  if j=i then * will be output
+                } else if (j == n - (i - 1)) 
+                {
                     System.out.print("*");
                 } else 
                 {
